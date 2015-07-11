be8cb3745cc01da21cdb8e07c317620a80c062ac
Jon Zielenkievicz <jonzielen@gmail.com>
2015-07-11 14:17:21 -0400

    add more test code to insert

--- routes/api.js
+++ routes/api.js
@@ -30,4 +30,33 @@ router.get('/api/:id', function(req, res, next) {
     });
 });
 
+router.get('/api/add', function(req, res, next) {
+    res.render('index', { title: '0+' });
+}).post('/api/add/:value', function() {
+    var value = req.param("value");
+
+    // List.findById(id, function(err, list) {
+    //   if (err) throw err;
+    //
+    //   res.jsonp({
+    //       data: list
+    //   });
+    // });
+
+
+    // Submit to the DB
+    List.insert({
+        "name" : 'test???'
+    }, function (err, doc) {
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            // And forward to success page
+            res.send(value + " added to the database.");
+        }
+    });
+});
+
 module.exports = router;
