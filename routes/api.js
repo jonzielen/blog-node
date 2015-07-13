4945fc1b9b354f899937701d77104146f29065c4
Jon Zielenkievicz <jonzielen@gmail.com>
2015-07-12 21:25:37 -0400

    add, save method to api

--- routes/api.js
+++ routes/api.js
@@ -18,44 +18,32 @@ router.get('/api', function(req, res, next) {
   res.render('index', { title: req.body.email });
 });
 
-router.get('/api/:id', function(req, res, next) {
-    var id = req.param("id");
-
-    List.findById(id, function(err, list) {
-      if (err) throw err;
-
-      res.jsonp({
-          data: list
-      });
-    });
-});
+// router.get('/api/:id', function(req, res, next) {
+//     var id = req.param("id");
+//
+//     List.findById(id, function(err, list) {
+//       if (err) throw err;
+//
+//       res.jsonp({
+//           data: list
+//       });
+//     });
+// });
 
 router.get('/api/add', function(req, res, next) {
-    res.render('index', { title: '0+' });
-}).post('/api/add/:value', function() {
-    var value = req.param("value");
+    res.render('add-blog', { title: '0+' });
+}).post('/api/add', function(req, res, next) {
+    var list = new List(); // create a new instance of the List model
+    list.name = req.body.email; // set the list name (comes from the request)
 
-    // List.findById(id, function(err, list) {
-    //   if (err) throw err;
-    //
-    //   res.jsonp({
-    //       data: list
-    //   });
-    // });
+    // save the list and check for errors
+    list.save(function(err) {
+        if (err)
+            res.send(err);
 
-
-    // Submit to the DB
-    List.insert({
-        "name" : 'test???'
-    }, function (err, doc) {
-        if (err) {
-            // If it failed, return error
-            res.send("There was a problem adding the information to the database.");
-        }
-        else {
-            // And forward to success page
-            res.send(value + " added to the database.");
-        }
+        res.json({
+            message: req.body.email+" added to the database."
+        });
     });
 });
 
