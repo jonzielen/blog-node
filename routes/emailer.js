8bf9847ce38e106a283a19c9d6bd31f856759cda
Jon Zielenkievicz <jzielenkievicz@nycgo.com>
2015-07-29 10:02:58 -0400

    add emailer link to header

--- routes/emailer.js
+++ routes/emailer.js
@@ -12,24 +12,29 @@ router.get('/', function(req, res, next) {
 }).post('/', function(req, res, next) {
 
     var transporter = nodemailer.createTransport(smtpTransport({
+        // works at work
         //host: 'mail.nycvb.nycvisit.com',
+        //port: 25,
+
+        // should be default fall back
         service: 'gmail',
         auth: {
             user: 'jonzielendevtests',
             pass: '#America'
         },
         port: 465,
+
         tls: {rejectUnauthorized: false},
         debug: true
     }));
 
     // setup e-mail data with unicode symbols
     var mailOptions = {
-        from:  req.body.from, // sender address
-        to:  req.body.to, // list of receivers
+        from: req.body.from, // sender address
+        to: req.body.to, // list of receivers
         subject: req.body.title, // Subject line
         //text: 'Hello world', // plaintext body
-        html:  req.body.body // html body
+        html: req.body.body // html body
     };
 
     // send mail with defined transport object
@@ -37,7 +42,7 @@ router.get('/', function(req, res, next) {
         if(error){
             return console.log(error);
         }
-        console.log('Message sent: ' + info.response);
+        console.log('info.response: ' + info.response);
 
     });
 
